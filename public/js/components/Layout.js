433792bb6a2381f7f0c4e65e9b07c99b7169c4f7
Sean Philippi <sean.philippi@gmail.com>
2019-02-23 18:59:33 -0600

    successful transfer of jsx from App to Layout functional component

--- public/js/components/Layout.js
+++ public/js/components/Layout.js
@@ -1,7 +1,37 @@
-import React from 'react';
-
-function Layout() {
-  return (
-    
-  )
+function Layout(props) {
+  const productDetails = props.products.map((p, i) => {
+    console.log('this.props', props)
+    return  <ProductDetail 
+    addToCart={props.addToCart}
+    key={i} 
+    product={p} />
+  });
+  return (  
+    <div className="App">
+      <Header cart={props.cart}/>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-3">
+            <p className="lead">Shop Name</p>
+              <div className="list-group">
+                <a href="#" className="list-group-item">Category 1</a>
+                <a href="#" className="list-group-item">Category 2</a>
+                <a href="#" className="list-group-item">Category 3</a>
+              </div>
+          </div>
+          {/*comments */}
+          <div className="col-md-9">
+            <Carousel />
+            <div className="row">
+                {productDetails}
+            </div>
+          </div>
+      </div>
+    </div>
+    <div className="container">
+      <hr/>
+      <Footer />
+    </div>
+  </div>
+  );
 }
